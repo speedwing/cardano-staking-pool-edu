e88266bf0df7571dd09549aaf2138297caef1ca3
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2020-11-07 22:32:10 +0000

    bla

--- cardano-node/bin/shelley-sendmytip.sh
+++ cardano-node/bin/shelley-sendmytip.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 if [[ -z "${CARDANO_POOL_ID}" ]]; then
-  echo "The required CARDANO_POOL_ID env var is not set. Exiting..."
+  echo "Required CARDANO_POOL_ID env var is not set. Exiting..."
   exit 1
 fi
 
 if [[ -z "${POOL_TOOL_API_KEY}" ]]; then
-  echo "The required POOL_TOOL_API_KEY env var is not set. Exiting..."
+  echo "Required POOL_TOOL_API_KEY env var is not set. Exiting..."
   exit 1
 fi
 
 if [[ -z "${NODE_VERSION}" ]]; then
-  echo "The required POOL_TOOL_API_KEY env var is not set. Exiting..."
+  echo "Required POOL_TOOL_API_KEY env var is not set. Exiting..."
   exit 1
 fi
 
@@ -31,6 +31,8 @@ shopt -s expand_aliases
 
 alias cli="$(which cardano-cli) shelley"
 
+lastBlockHeightSeen="0"
+
 while true ; do
   sleep 1
   nodeTip=$(cli query tip --testnet-magic 1097911063)
@@ -38,11 +40,13 @@ while true ; do
   lastBlockHash=$(echo ${nodeTip} | jq -r .headerHash)
   lastBlockHeight=$(echo ${nodeTip} | jq -r .blockNo)
 
-  JSON="$(jq -n --compact-output --arg NODE_ID "$PT_MY_NODE_ID" --arg MY_API_KEY "$PT_MY_API_KEY" --arg MY_POOL_ID "$PT_MY_POOL_ID" --arg VERSION "$NODE_VERSION" --arg BLOCKNO "$lastBlockHeight" --arg SLOTNO "$lastSlot" --arg BLOCKHASH "$lastBlockHash" '{apiKey: $MY_API_KEY, poolId: $MY_POOL_ID, data: {nodeId: $NODE_ID, version: $VERSION, blockNo: $BLOCKNO, slotNo: $SLOTNO, blockHash: $BLOCKHASH}}')"
-  echo "Packet Sent: $JSON"
 
-  if [ "${lastBlockHeight}" != "" ]; then
-  RESPONSE="$(curl -s -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "$JSON" "https://api.pooltool.io/v0/sendstats")"
-  echo $RESPONSE
+  if [ "${lastBlockHeight}" != "" ] && [ "${lastBlockHeight}" -gt "${lastBlockHeightSeen}" ]; then
+    echo "New height"
+    JSON="$(jq -n --compact-output --arg NODE_ID "$PT_MY_NODE_ID" --arg MY_API_KEY "$PT_MY_API_KEY" --arg MY_POOL_ID "$PT_MY_POOL_ID" --arg VERSION "$NODE_VERSION" --arg BLOCKNO "$lastBlockHeight" --arg SLOTNO "$lastSlot" --arg BLOCKHASH "$lastBlockHash" '{apiKey: $MY_API_KEY, poolId: $MY_POOL_ID, data: {nodeId: $NODE_ID, version: $VERSION, blockNo: $BLOCKNO, slotNo: $SLOTNO, blockHash: $BLOCKHASH}}')"
+    echo "Packet Sent: $JSON"
+    RESPONSE="$(curl -s -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "$JSON" "https://api.pooltool.io/v0/sendstats" || true)"
+    echo "${RESPONSE}"
+    lastBlockHeightSeen="${lastBlockHeight}"
   fi
 done
