c6371233464bbacdaaf19de6f3f66e7e454a13b7
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2020-11-09 15:50:14 +0000

    bla

--- utils/build.sh
+++ utils/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 VERSION=0.1
 
 GIT_COMMIT_SHA=$(git describe --always)
@@ -7,7 +9,7 @@ GIT_COMMIT_SHA=$(git describe --always)
 TAG="${VERSION}-${GIT_COMMIT_SHA}"
 
 if [ "$1" = "arm64" ]; then
-  docker buildx build --push -t speedwing/cardano-node-utils:"${TAG}" .
+  docker buildx build --push --platform linux/arm64 -t speedwing/cardano-node-utils:"${TAG}" .
 else
   docker build -t speedwing/cardano-node-utils:"${TAG}" .
   docker push -t speedwing/cardano-node-utils:"${TAG}"
