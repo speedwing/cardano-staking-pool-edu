7c8f029cd30558edda1208699817998685c3d16b
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2020-11-26 16:34:23 +0000

    grafana

--- deploy/01-sealed-secrets/install.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-helm upgrade --install --namespace kube-system sealed-secrets stable/sealed-secrets
--- deploy/03-kube-prometheus-stack/install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-helm upgrade --install kube-prometheus-stack -n observe prometheus-community/kube-prometheus-stack -f values-rpi.yaml
