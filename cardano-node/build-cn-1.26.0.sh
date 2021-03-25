4ec7d903aa14c01eea639c6a9d0aa4dccab0fdc8
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2021-03-25 15:47:52 +0000

    updates

---
+++ cardano-node/build-cn-1.26.0.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+
+OS_ARCH=$(uname -m)
+
+CARDANO_NODE_VERSION=1.26.0
+
+CARDANO_NODE_IMAGE_TAG="${CARDANO_NODE_VERSION}-${OS_ARCH}"
+
+docker build -t cardano-node:"${CARDANO_NODE_IMAGE_TAG}" \
+  --build-arg GHC_VERSION=8.10.2 \
+  --build-arg OS_ARCH="${OS_ARCH}" \
+  --build-arg CARDANO_VERSION=${CARDANO_NODE_VERSION} \
+  -f "${CARDANO_NODE_VERSION}-${OS_ARCH}.dockerfile" .
