713391d335028fc3d7d69ebd8865e01156624501
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2021-04-30 16:56:05 +0100

    stuff

---
+++ misc/atada/extract-cardano-binaries.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+OS_ARCH=$(uname -m)
+CARDANO_NODE_VERSION=${CARDANO_NODE_VERSION:-1.26.2}
+
+set -x
+
+mkdir -p ~/tmp/cardano-bin || return 0
+
+docker run -it --rm \
+  -v "/home/ubuntu/tmp/cardano-bin:/tmp cardano-node:${CARDANO_NODE_VERSION}-${OS_ARCH}"
+  "cp /usr/local/bin/cardano-cli /tmp/cardano-cli; cp /usr/local/bin/cardano-node /tmp/cardano-cli"
+
