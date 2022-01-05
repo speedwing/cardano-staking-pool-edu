dd1bd04ff31d14a16a25774b325cac0ec0c06d14
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2022-01-05 20:27:40 +0000

    first step for new way of building node

---
+++ cardano-node/1.33.0-aarch64.dockerfile
@@ -0,0 +1,26 @@
+FROM ubuntu:20.04 as builder
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update \
+    && apt-get upgrade -y curl zip
+
+WORKDIR /cardano-node
+RUN curl -LO https://ci.zw3rk.com/build/427926/download/1/aarch64-unknown-linux-musl-cardano-node-1.33.0.zip && \
+    unzip aarch64-unknown-linux-musl-cardano-node-1.33.0.zip
+
+FROM ubuntu:20.04
+
+COPY config /etc/config
+
+COPY --from=builder /cardano-node/cardano-node/cardano-node /usr/local/bin/
+COPY --from=builder /cardano-node/cardano-node/cardano-cli /usr/local/bin/
+
+## Attempt to check on the prometheus metrics port if the node is up and running
+HEALTHCHECK --interval=10s --timeout=60s --start-period=300s --retries=3 CMD curl -f http://localhost:12798/metrics || exit 1
+
+RUN useradd -ms /bin/bash cardano
+USER cardano
+WORKDIR /home/cardano
+
+ENTRYPOINT ["bash", "-c"]
---
+++ cardano-node/dl-config.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+CARDANO_NODE_CONF_FOLDER=config
+
+download_config() {
+  rm -fr config
+  mkdir -p "${CARDANO_NODE_CONF_FOLDER}" && cd "${CARDANO_NODE_CONF_FOLDER}" \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-config.json && \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-byron-genesis.json && \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-shelley-genesis.json && \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-alonzo-genesis.json && \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-topology.json && \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-config.json && \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-byron-genesis.json && \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-shelley-genesis.json && \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-alonzo-genesis.json && \
+    curl -LO https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-topology.json && \
+    cd ..
+}
+
+rm_config_folder() {
+  rm -fr config
+}
+
