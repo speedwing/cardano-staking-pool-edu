3fa43ff693702d6b6699f2e3245e7b77ec29aad6
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2022-08-27 21:53:17 +0100

    exit 0

--- cardano-node/run-relay.sh
+++ cardano-node/run-relay.sh
@@ -2,4 +2,5 @@
 
 export NETWORK=testnet
 
-docker run --name cardano-node-testnet -v ~/cardano-node/testnet:/data/db -e NETWORK=testnet cardano-node:1.35.2-x86_64
+docker rm cardano-node-testnet || true
+docker run --name cardano-node-testnet -d -v ~/cardano-node/testnet:/data/db -e NETWORK=testnet cardano-node:1.35.2-x86_64
